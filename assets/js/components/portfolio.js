e2e72890173e7c7a04857db88d70fe4d4688032d
Tausif Ansari <139870807+tausif64@users.noreply.github.com>
2023-10-27 17:02:54 +0530

    Update portfolio.js

--- assets/js/components/portfolio.js
+++ assets/js/components/portfolio.js
@@ -7,7 +7,7 @@ const portfolio = {
           <div class="row w-100 p-3">
             <div class="col-lg-6 col-md-12 col-sm-12 px-lg-5 border border-start-0 border-top-0 border-bottom-0">
                 <div class="card mt-3 bg-transparent">
-                  <img src="/assets/img/shiva.png" height="300px" alt="Project 1" />
+                  <img src="/tausif-portfolio/assets/img/shiva.png" height="300px" alt="Project 1" />
                   <div class="p-3 p-details">
                     <div class="mb-2 mt-3">Project Name : <a target="_blank" href="https://shivaenvirolab.com/">SHIVA ENVIRO LAB AND RESEARCH CENTRE</a></div>
                     <div class="mb-2">My role : Front End Developer</div>
@@ -19,7 +19,7 @@ const portfolio = {
             </div>
             <div class="col-lg-6 col-md-12 col-sm-12 px-5">
                 <div class="card mt-3 bg-transparent">
-                    <img src="/assets/img/idea.png" height="300px" alt="Project 2" />
+                    <img src="/tausif-portfolio/assets/img/idea.png" height="300px" alt="Project 2" />
                       <div class="p-3 p-details">
                         <div class="mb-2 mt-3">Project Name : <a target="_blank" href="http://www.idea4homes.com/">IDEA 4 HOMES</a></div>
                         <div class="mb-2">My role : Front End Developer</div>
@@ -31,7 +31,7 @@ const portfolio = {
             </div>
             <div class="col-lg-6 col-md-12 col-sm-12 px-5">
                 <div class="card mt-3 bg-transparent">
-                    <img src="/assets/img/project-3.png" height="300px" alt="Project 3" />
+                    <img src="/tausif-portfolio/assets/img/project-3.png" height="300px" alt="Project 3" />
                       <div class="p-3 p-details">
                         <div class="mb-2 mt-3">Project Name : <a target="_blank" href="https://modelschoolkanke.com/">CM SCHOOL OF EXELLENCE</a></div>
                         <div class="mb-2">My role : Front End Developer</div>
