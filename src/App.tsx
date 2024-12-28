d7286623c27927c3a4e21c23abd158b94b60b254
tausif64 <developer.642003@gmail.com>
2024-12-28 22:34:27 +0530

    Sixth commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ProjectDetail from "./pages/ProjectDetails";
 import CustomCursor from "./components/CustomCursor";
 import Resume from "./pages/Resume";
 import Contact from "./pages/Contact";
+import NotFoundPage from "./pages/NotFoundPage";
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/tausif-portfolio/resume" element={<Resume />} />
         <Route path="/tausif-portfolio/contact" element={<Contact />} />
         <Route path="/tausif-portfolio/projects/:id" element={<ProjectDetail />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   )
